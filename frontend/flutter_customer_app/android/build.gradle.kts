33109917c83e829d3c45c80679c7d883da98d6c1
honey728989 <imhimanshurs.com>
2026-03-23 15:13:04 +0530

    Suppress Android Java compile warnings

--- frontend/flutter_customer_app/android/build.gradle.kts
+++ frontend/flutter_customer_app/android/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.tasks.compile.JavaCompile
+
 allprojects {
     repositories {
         google()
@@ -17,6 +19,17 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        options.compilerArgs.addAll(
+            listOf(
+                "-Xlint:-options",
+                "-Xlint:-deprecation",
+            ),
+        )
+    }
 }
 
 tasks.register<Delete>("clean") {
